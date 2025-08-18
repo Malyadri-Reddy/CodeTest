c2a850304bc29b28299ffacc7e85c92faac96a3e
Goutham <gk.svgk@gmail.com>
2025-08-18 10:04:22 +0530

    Initial Commit

---
+++ CodeTest/CodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTestApp.swift
+//  CodeTest
+//
+//  Created by Goutham S on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
